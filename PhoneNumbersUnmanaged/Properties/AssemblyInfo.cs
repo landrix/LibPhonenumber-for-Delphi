0b677ed6af044ce56c0e0b7a713f46f91268afa4
Sven Harazim <sven.harazim@landrix.de>
2021-05-17 07:57:10 +0200

    v8.12.23

--- PhoneNumbersUnmanaged/Properties/AssemblyInfo.cs
+++ PhoneNumbersUnmanaged/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.12.22.0")]
-[assembly: AssemblyFileVersion("8.12.22.0")]
+[assembly: AssemblyVersion("8.12.23.0")]
+[assembly: AssemblyFileVersion("8.12.23.0")]
