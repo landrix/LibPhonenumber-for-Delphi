132a86084f4727811db56c4bd02e3ddc336eb9d2
Sven Harazim <info@landrix.de>
2022-01-06 15:45:53 +0100

    v8.12.38

--- PhoneNumbersUnmanaged/Properties/AssemblyInfo.cs
+++ PhoneNumbersUnmanaged/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sven Harazim")]
-[assembly: AssemblyCopyright("Copyright © 2021")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("8.12.34.0")]
-[assembly: AssemblyFileVersion("8.12.34.0")]
+[assembly: AssemblyVersion("8.12.38.0")]
+[assembly: AssemblyFileVersion("8.12.38.0")]
