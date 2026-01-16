cf5002bb9a9f1aa5240615a7e7708df05e93d378
Sven Harazim <info@landrix.de>
2026-01-16 12:26:34 +0100

    V9.0.22

--- PhoneNumbersUnmanaged/Properties/AssemblyInfo.cs
+++ PhoneNumbersUnmanaged/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("9.0.21.0")]
-[assembly: AssemblyFileVersion("9.0.21.0")]
+[assembly: AssemblyVersion("9.0.22.0")]
+[assembly: AssemblyFileVersion("9.0.22.0")]
